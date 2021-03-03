012c229c9e7fe0730ef3a2744b3b0fc7ca542d14
Philip A Grim II <Philip A Grim II@Fortress>
2021-03-03 12:00:28 -0500

    Added prefix for testnet.  Added boolean parameter to constructor to determine main or test.

--- src/main/java/live/thought/scatterbrain/PrivateKey.java
+++ src/main/java/live/thought/scatterbrain/PrivateKey.java
@@ -9,14 +9,15 @@ public class PrivateKey
   private static final SecureRandom secureRandom = new SecureRandom();
   private static final int KEY_SIZE = 32;
   private static final int KEY_VER = 123;
+  private static final int TEST_KEY_VER = 235;
   private String key;
    
-  public PrivateKey()
+  public PrivateKey(boolean testnet)
   {
     byte[] data = new byte[KEY_SIZE];
     secureRandom.nextBytes(data);
     Sha256Hash hash = Sha256Hash.of(data);
-    key = Base58.encodeChecked(KEY_VER, hash.getBytes());
+    key = Base58.encodeChecked(testnet?TEST_KEY_VER:KEY_VER, hash.getBytes());
   }
   
   @Override
