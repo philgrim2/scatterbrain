a568e7c3673581880821b91512c2be7e344b86f0
Philip A Grim II <Philip A Grim II@Fortress>
2021-03-03 12:01:32 -0500

    Updated constructor for testnet boolean.

--- src/test/java/live/thought/scatterbrain/PrivateKeyTest.java
+++ src/test/java/live/thought/scatterbrain/PrivateKeyTest.java
@@ -10,7 +10,7 @@ public class PrivateKeyTest
   @Test
   public void test()
   {
-    PrivateKey testKey = new PrivateKey();
+    PrivateKey testKey = new PrivateKey(false);
     assertNotNull(testKey);
     System.out.println(testKey);
   }
